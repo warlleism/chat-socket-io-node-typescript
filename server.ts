a49898a5098dadd43ec43794d38f5b4aa8ba3574
Warllei <warllei.s.martins@hotmail.com>
2024-09-05 08:44:24 -0300

    feat: agora renderizam videos

--- server.ts
+++ server.ts
@@ -5,11 +5,17 @@ import { Server } from 'socket.io';
 
 const app = express();
 const httpserver = http.createServer(app);
-const io = new Server(httpserver);
+
+const io = new Server(httpserver, {
+    maxHttpBufferSize: 1e8
+});
 
 httpserver.listen(3000, () => console.log('server is running'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/view/index.html'));
@@ -23,21 +29,29 @@ const users = {
     "5": { id: "5", img: "https://images.pexels.com/photos/769690/pexels-photo-769690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", phone: "(51)945678901", email: "everaldo@example.com", username: "Everaldo", status: "online" }
 };
 
-const userSockets = {};
-const privateChats = {};
-const offlineMessages = {};
+const userSockets: any = {};
+const privateChats: any = {};
+const offlineMessages: any = {};
 
 io.on('connection', (socket) => {
-    // console.log('user connected', socket.id);
-
     const userId = Object.keys(users)[Math.floor(Math.random() * 5)];
     userSockets[userId] = socket.id;
 
     const userName = users[userId].username;
 
-    socket.emit('userDetails', { id: userId, email: users[userId].email, phone: users[userId].phone, username: userName, img: users[userId].img, status: users[userId].status });
+    socket.emit('userDetails', {
+        id: userId,
+        email: users[userId].email,
+        phone: users[userId].phone,
+        username: userName,
+        img: users[userId].img,
+        status: users[userId].status
+    });
+
+    // Atualizar lista de usuários para todos
     io.emit('updateUsers', Object.values(users));
 
+    // Enviar mensagens offline, se existirem
     if (offlineMessages[userId]) {
         offlineMessages[userId].forEach((msg: any) => {
             socket.emit('message', msg);
@@ -45,18 +59,25 @@ io.on('connection', (socket) => {
         delete offlineMessages[userId];
     }
 
+    // Lógica para mensagens
     socket.on('message', (msg, targetId = null) => {
-
         const now = new Date();
         const date = now.toLocaleDateString();
         const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
         const timestamp = `${date} ${time}`;
 
-        const message = { username: users[userId].username, msg, timestamp, userId, targetId, img: users[userId].img };
+        // Mensagem pode conter texto ou base64 de imagem
+        const message = {
+            username: users[userId].username,
+            msg, // Aqui vai a mensagem, que pode ser em base64
+            timestamp,
+            userId,
+            targetId,
+            img: users[userId].img
+        };
 
         if (targetId) {
             const targetSocketId = userSockets[targetId];
-            console.log(targetSocketId)
             if (targetSocketId) {
                 io.to(targetSocketId).emit('message', message);
                 socket.emit('message', message);
@@ -67,14 +88,18 @@ io.on('connection', (socket) => {
                 }
                 offlineMessages[targetId].push(message);
             }
+        } else {
+            io.emit('message', message);
         }
     });
 
+    // Iniciar chat privado
     socket.on('startPrivateChat', (targetId) => {
         privateChats[socket.id] = targetId;
         privateChats[targetId] = socket.id;
     });
 
+    // Desconectar usuário
     socket.on('disconnect', () => {
         console.log('user disconnected');
         delete userSockets[userId];
